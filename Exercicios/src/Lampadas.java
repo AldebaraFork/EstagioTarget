52fec8013eb4d7d1d613dfeab8ea12b9dc793498
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2024-09-04 17:17:48 -0300

    Lampadas

---
+++ Exercicios/src/Lampadas.java
@@ -0,0 +1,11 @@
+public class Lampadas {
+    public static void main(String[] args) {
+//        Primeira Ida: Ligue dois dos três interruptores e espere alguns minutos.
+//       Segunda Ida: Desligue um dos interruptores ligados e vá até as salas.
+        
+//        Análise:
+//        Lâmpada acesa e quente: O interruptor que ficou ligado.
+//          Lâmpada acesa e fria: O interruptor que foi ligado e desligado.
+//             Lâmpada apagada: O interruptor que não foi tocado.
+    }
+}
