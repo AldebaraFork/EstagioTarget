667cd51d2210f246f77a650f543e175899a70152
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2024-09-04 17:15:55 -0300

    Logica

---
+++ Exercicios/src/Logica.java
@@ -0,0 +1,36 @@
+public class Logica {
+
+    //ESTE SERA UM EXERCICIO COMENTADO
+
+
+//    a) 1, 3, 5, 7, ___
+//    Lógica: numeros consecutivos.
+//    Próximo numero: 9
+
+
+//    b) 2, 4, 8, 16, 32, 64, ____
+//    Lógica: Sequência de potênciação de 2.
+//    Próximo numero: 128
+
+
+//    c) 0, 1, 4, 9, 16, 25, 36, ____
+//    Lógica: quadrados perfeitos (0², 1², 2², 3², ...).
+//    Próximo numero: 49
+
+
+//    d) 4, 16, 36, 64, ____
+//    Lógica: numeros pares ao quadrado(2², 4², 6², 8²).
+//    Próximo numero: 100 (10²)
+
+
+//    e) 1, 1, 2, 3, 5, 8, ____
+//    Lógica: Sequência de Fibonacci .
+//    Próximo numero: 13
+
+
+//    f) 2,10, 12, 16, 17, 18, 19, ____
+//    Lógica: Não entendi a sequencia.
+//            Conclusão:
+//
+
+}
