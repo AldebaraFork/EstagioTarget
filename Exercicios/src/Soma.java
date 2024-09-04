53ceb649631782a25694869a4c01e2eecabe9eab
Eduardo Pereira <145592635+AldebaraFork@users.noreply.github.com>
2024-09-04 17:12:18 -0300

    Soma

---
+++ Exercicios/src/Soma.java
@@ -0,0 +1,12 @@
+public class Soma {
+
+    public static void main(String[] args) {
+        int INDICE = 12, SOMA = 0, K = 1;
+        while(K < INDICE){
+            K ++;
+            SOMA += K;
+
+        }
+        System.out.println(SOMA);
+    }
+}
